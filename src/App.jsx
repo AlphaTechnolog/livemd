b3ef3aadf1663e7bc64b8b7c95b2f459378c3fac
Gabriel <gfranklings@gmail.com>
2021-10-17 22:03:39 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <>
+      <h1>Hello, World from App.jsx</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import { render } from 'react-dom'
+import App from './App'
+
+const location = document.getElementById('root')
+
+render(
+  <App />,
+  location
+)
